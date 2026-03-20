b99cfbe341ea84067b2ad4489c35c8c56a79e4a5
binottowendel-code <binottowendel@gmail.com>
2026-03-20 12:40:53 -0300

    Treinamento de js(node)

---
+++ exercicios basicos/saidadevariavel.js
@@ -0,0 +1,22 @@
+//Variavei e saida\\
+/*let nome, idade;
+
+let nome = "Wendel"
+let idade = 20
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos")
+*/
+//soma de dois numeros\\
+/*
+let num1 = 10;
+let num2 = 5;
+let soma = num1 + num2;
+console.log("A soma dos 2 numeros é: ", soma, );
+*/
+//Exercicio dobro de um numero\\
+/*
+let num1 = 9;
+let dobro = num1 * 2;
+console.log("O dobro desse numero é: ", dobro );
+*/
+//Exercicio converter idade\\

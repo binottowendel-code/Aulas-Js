67b55395fd9bbe2c1a89c3eda94d4684bf51b019
binottowendel-code <binottowendel@gmail.com>
2026-05-11 17:14:52 -0300

    Create inverter-ordem-array.js

---
+++ exercicios de array/inverter-ordem-array.js
@@ -0,0 +1,7 @@
+//exerciico para inverter ordem do array
+num = [1,2,3,4,5,6,7,8,9,10];
+numinvertido = []
+for ( i = num.length - 1; i >= 0 ; i--){
+    numinvertido.push(num[i])
+}
+console.log(numinvertido)
